337d8407bf00d52d523cc2a6b07a4240c8f24208
ptl-ptl <155249971+ptl-ptl@users.noreply.github.com>
2024-05-26 15:40:16 +0200

    Added basic form handling and added Typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-//alert("Hello World!");
+function generatePoem(event) {
+  event.preventDefault();
+
+  new Typewriter("#display-poem", {
+    strings: "I come with no wrapping or pretty pink bows",
+    autoStart: true,
+    cursor: null,
+    delay: 20,
+  });
+
+  /*let apiKey = "67b3fe199e6a5df80a44a5o53t65b217";
+  let prompt = ;
+  let context = "You are ";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;*/
+
+  //xios.get(apiUrl).then(showPoem);
+}
+
+let poemFormElement = document.querySelector("#poem-form");
+poemFormElement.addEventListener("submit", generatePoem);
